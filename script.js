502fafeaa6e4dfdb7053271427c4a73b826f9c8a
paud-kb <ksdoel27@gmail.com>
2025-09-26 16:36:18 +0700

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// Fungsi alert sederhana
 function halo() {
   alert("Halo dari KB Melati 1!");
 }
+
+// TikTok Embed
+(function loadTikTokEmbed() {
+  // Cek apakah script TikTok sudah ada
+  if (!document.querySelector('script[src="https://www.tiktok.com/embed.js"]')) {
+    const script = document.createElement('script');
+    script.src = "https://www.tiktok.com/embed.js";
+    script.async = true;
+    document.body.appendChild(script);
+  }
+})();
